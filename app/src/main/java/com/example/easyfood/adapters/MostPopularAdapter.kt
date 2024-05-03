4f8ef16a52136a1f5857d77c0cfb55bde7dd59c8
Waqas Ahmed <wa02263@gmail.com>
2024-05-03 11:20:19 +0500

    detail_activity added

--- app/src/main/java/com/example/easyfood/adapters/MostPopularAdapter.kt
+++ app/src/main/java/com/example/easyfood/adapters/MostPopularAdapter.kt
@@ -1,3 +1,23 @@
 package com.example.easyfood.adapters
 
-class MostPopularAdapter {}
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.easyfood.databinding.PopularItemsBinding
+
+class MostPopularAdapter():RecyclerView.Adapter<MostPopularAdapter.PopularViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    class PopularViewHolder(val binding: PopularItemsBinding):RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.easyfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
+import com.example.easyfood.activities.MealActivity
 import com.example.easyfood.databinding.FragmentHomeBinding
 import com.example.projo.Meal
 import com.example.viewModel.HomeViewModel
@@ -35,8 +37,16 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         homeMvvm.getRandomMeal()
         observeRandomMeal()
+        onRandomMealClick()
 
         }
+
+    private fun onRandomMealClick() {
+        binding.imgRandomMeal.setOnClickListener{
+            val intent=Intent(activity,MealActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     private fun observeRandomMeal() {
         homeMvvm.observeRandomMealLiveData().observe(/* owner = */ viewLifecycleOwner,/* observer = */
